42be963d478bfcb9e4ebe7eb633561b7ca62d1c4
unknown <kenjibujiwaratamagotchi@gmail.com>
2026-01-08 10:21:48 +0800

    first commit

---
+++ lib/screens/auth/signin_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SigninScreen extends StatefulWidget {
+  const SigninScreen({super.key});
+
+  @override
+  State<SigninScreen> createState() => _SigninScreenState();
+}
+
+class _SigninScreenState extends State<SigninScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+          // LOGO
+
+          // Welcome Message
+
+          // Username TextField
+
+          // Password TextField
+
+          // Sign In Button
+
+          // Sign Up TextButton
+        ],
+      )
+    );
+  }
+}
